b2c814f089b42b339d2e80e73f51b385e577283d
Arun Prasaad <arunbear@cpan.org>
2025-10-27 15:17:00 +0000

    Add back node types

--- migrations/1/up.sql
+++ migrations/1/up.sql
@@ -47,6 +47,11 @@ CREATE INDEX idx_note_path ON note USING GIST (path);
 INSERT INTO monk (id, username, created_at, updated_at)
 VALUES (961, 'Anonymous Monk', NOW(), NOW());
 
+-- Add node types
+INSERT INTO node_type (id, name) VALUES
+(11, 'note'),
+(115, 'perlquestion');
+
 -- Function to update the updated_at timestamp
 CREATE OR REPLACE FUNCTION update_modified_column()
 RETURNS TRIGGER AS $$
