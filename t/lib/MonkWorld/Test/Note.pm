d03f2802f536a1c712fc9e5b710ecc4a78e92925
Arun Prasaad <arunbear@cpan.org>
2025-10-29 14:25:36 +0000

    Verify creating a note with id

--- t/lib/MonkWorld/Test/Note.pm
+++ t/lib/MonkWorld/Test/Note.pm
@@ -7,8 +7,7 @@ use MonkWorld::API::Constants qw(NODE_TYPE_NOTE NODE_TYPE_PERLQUESTION);
 use Test::Class::Most
   parent => 'MonkWorld::Test::Base';
 
-
-sub a_note_can_be_created : Test(3) ($self) {
+sub a_note_can_be_created : Test(4) ($self) {
     my $t = $self->mojo;
 
     my $auth_token = $ENV{MONKWORLD_AUTH_TOKEN}
@@ -55,5 +54,36 @@ sub a_note_can_be_created : Test(3) ($self) {
         ->json_is('/parent_node' => $parent_node_id)
         ->json_is('/path' => "$parent_node_id." . $t->tx->res->json->{id})
         ;
+    };
+
+    subtest 'with an explicit ID' => sub {
+        my $id = $t->tx->res->json->{id};
+        ok $id > 0, 'ID is a positive integer';
+        my $explicit_id = $id + 2; # better than 1 as auto increment would give a false pass
+
+        $t->post_ok(
+            '/node' => {
+                'Authorization' => "Bearer $auth_token"
+            } => json => {
+                node_id      => $explicit_id,
+                node_type_id => NODE_TYPE_NOTE,
+                author_id    => $anon_user_id,
+                title        => 'Test Note with ID',
+                doctext      => 'This is a test note with explicit ID',
+                root_node    => $root_node_id,
+                parent_node  => $parent_node_id,
+                created      => $created_at,
+            }
+        )
+        ->status_is(HTTP_CREATED)
+        ->header_like('Location' => qr{/node/$explicit_id$})
+        ->json_is('/id' => $explicit_id)
+        ->json_has('/created_at')
+        ->json_is('/title' => 'Test Note with ID')
+        ->json_is('/doctext' => 'This is a test note with explicit ID')
+        ->json_is('/node_type_id' => NODE_TYPE_NOTE)
+        ->json_is('/root_node' => $root_node_id)
+        ->json_is('/parent_node' => $parent_node_id)
+        ->json_is('/path' => "$parent_node_id.$explicit_id");
     };
 }
